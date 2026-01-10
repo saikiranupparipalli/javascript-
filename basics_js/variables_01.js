61c28a3a51437f60103461571ae2a973f3a891d9
Sai kiran <saikiran29.u@gmail.com>
2026-01-10 21:18:53 +0530

    files updated

--- basics_js/variables_01.js
+++
@@ -1,9 +0,0 @@
-const accountId=1234;
-let accountEmail="hello.gmail.com";
-let accountcity="hyderabad";
-console.table([accountId,accountEmail,accountcity]);
-
-let name = "saikiran"
-let rooNum = 544
-let branch = "CSS"
-console.table([name,rooNum,branch])

ea9503e13fd37fb377bd3e1bfc7b6b8f3fe6ba87
Sai kiran <saikiran29.u@gmail.com>
2026-02-03 15:18:23 +0530

    todolist project with toggle

--- index.js
+++ index.js
@@ -12,20 +12,67 @@
 //  const num = parseInt('mk')
 //  console.log(num)
 
-function add(a,b){
-   console.log(a + b) 
+// function add(a,b){
+//    console.log(a + b) 
+// }
+// add(8,8)
+
+// const name = 'sai kiran from cs undergraduate'
+// console.log(name.split(" "))
+// console.log(name[2])
+
+
+// const myString = "Hello World. How are you doing?";
+// const splits = myString.split(" ", 3);
+
+// console.log(splits); // [ "Hello", "World.", "How" ]
+
+// const split = 'cs under graduate'
+// console.log(split.split(" "))
+
+const inputText = document.querySelector('input[type="text"]');
+const button = document.getElementById('add-btn');
+const pendingtask = document.getElementById('pending-count');
+const todolist = document.getElementById('todo-list');
+
+let storeTask = [];
+
+// ADD TASK
+button.addEventListener('click', () => {
+  const userInput = inputText.value.trim();
+
+  if (userInput === '') {
+    alert('Please enter the task');
+    return;
+  }
+
+  storeTask.push({ task: userInput });
+  inputText.value = '';
+
+  renderTasks();
+});
+
+// DELETE TASK (EVENT DELEGATION)
+todolist.addEventListener('click', (event) => {
+  if (event.target.classList.contains('btn')) {
+    const index = event.target.dataset.index;
+    storeTask.splice(index, 1);
+    renderTasks();
+  }
+});
+
+// RENDER TASKS
+function renderTasks() {
+  todolist.innerHTML = '';
+
+  storeTask.forEach((item, index) => {
+    todolist.innerHTML += `
+      <li class="item">
+        <span>${item.task}</span>
+        <button class="btn" data-index="${index}">✖</button>
+      </li>
+    `;
+  });
+
+  pendingtask.textContent = storeTask.length;
 }
-add(8,8)
-
-const name = 'sai kiran from cs undergraduate'
-console.log(name.split(" "))
-console.log(name[2])
-
-
-const myString = "Hello World. How are you doing?";
-const splits = myString.split(" ", 3);
-
-console.log(splits); // [ "Hello", "World.", "How" ]
-
-const split = 'cs under graduate'
-console.log(split.split(" "))
--- projects/to-do-list/index.js
+++ projects/to-do-list/index.js
@@ -1,3 +1,48 @@
+// const inputText = document.querySelector('input[type="text"]')
+
+// const button = document.getElementById('add-btn')
+// let pendingtask = document.getElementById('pending-count')
+// let todolist = document.getElementById('todo-list')
+
+// let storeTask = []
+// let clearbutton = document.createElement('button')
+
+// button
+// .addEventListener('click', () => {
+//     let userInput = inputText.value.trim()
+//     if(userInput == ''){
+//         alert('Please enter the task')
+//     } else {
+//         storeTask.push({task: userInput})
+//         // console.log(userInput)
+//         console.log(storeTask)
+//         inputText.value = ''
+//         renderTask()
+//     }
+
+// })
+
+// todolist.addEventListener('click', (event) => {
+//     if(event.target.classList.contains("btn")){
+//         const index = event.target.dataset.index
+//         storeTask.splice(index, 1)
+//         renderTask()
+//     }
+// })
+
+// function renderTask(){
+//     todolist.innerHTML = ''
+    
+//     storeTask.forEach((item, index) => {
+//         todolist.innerHTML +=  `<li class = item> <span>${item.task} </span>
+//          <button class = "btn" data-index = "${index}">✖</button> </li> `
+//     })
+//     pendingtask.textContent = storeTask.length
+// }
+
+
+// scratch-out the completed task.
+
 const inputText = document.querySelector('input[type="text"]')
 
 const button = document.getElementById('add-btn')
@@ -9,24 +54,34 @@ let clearbutton = document.createElement('button')
 
 button
 .addEventListener('click', () => {
-    let userInput = inputText.value
+    let userInput = inputText.value.trim()
     if(userInput == ''){
         alert('Please enter the task')
     } else {
-        userInput = userInput.trim()
-        storeTask.push({task: inputText.value})
-        console.log(userInput)
+        storeTask.push({task: userInput, completed: false})
+        // console.log(userInput)
         console.log(storeTask)
         inputText.value = ''
-        
+        renderTask()
     }
-    todolist.innerHTML = ''
 
-    
-    storeTask.forEach((item) =>{
-        todolist.innerHTML += `<li class = "item"> <span>${item.task} </span> <button class ='btn'>✖</button </li>`
-    })
-    if(storeTask.task == ''){
-        
+})
+
+todolist.addEventListener('click', (event) => {
+    if(event.target.classList.contains("btn")){
+        const index = event.target.dataset.index
+        storeTask[index].completed = !storeTask[index].completed
+        renderTask()
     }
 })
+
+function renderTask(){
+    todolist.innerHTML = ''
+    
+    storeTask.forEach((item, index) => {
+        const completedClass = item.completed ? 'completed' : '';
+        todolist.innerHTML +=  `<li class = "item ${completedClass}"> <span>${item.task} </span>
+         <button class = "btn" data-index = "${index}">✖</button> </li> `
+    })
+    pendingtask.textContent = storeTask.length
+}

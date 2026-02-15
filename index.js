8aaaa02dbc8edfb54e85ec1a6ceb67fd9e08cbbd
Sai kiran <saikiran29.u@gmail.com>
2026-02-15 23:35:55 +0530

    index4.html updated practice file

--- index.js
+++ index.js
@@ -30,49 +30,7 @@
 // const split = 'cs under graduate'
 // console.log(split.split(" "))
 
-const inputText = document.querySelector('input[type="text"]');
-const button = document.getElementById('add-btn');
-const pendingtask = document.getElementById('pending-count');
-const todolist = document.getElementById('todo-list');
-
-let storeTask = [];
-
-// ADD TASK
-button.addEventListener('click', () => {
-  const userInput = inputText.value.trim();
-
-  if (userInput === '') {
-    alert('Please enter the task');
-    return;
-  }
-
-  storeTask.push({ task: userInput });
-  inputText.value = '';
-
-  renderTasks();
-});
-
-// DELETE TASK (EVENT DELEGATION)
-todolist.addEventListener('click', (event) => {
-  if (event.target.classList.contains('btn')) {
-    const index = event.target.dataset.index;
-    storeTask.splice(index, 1);
-    renderTasks();
-  }
-});
-
-// RENDER TASKS
-function renderTasks() {
-  todolist.innerHTML = '';
-
-  storeTask.forEach((item, index) => {
-    todolist.innerHTML += `
-      <li class="item">
-        <span>${item.task}</span>
-        <button class="btn" data-index="${index}">✖</button>
-      </li>
-    `;
-  });
-
-  pendingtask.textContent = storeTask.length;
-}
+const a = 4
+const b = 4
+let c = null
+console.log( c = a + b)
